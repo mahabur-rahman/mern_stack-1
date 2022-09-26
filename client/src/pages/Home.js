8ada4f38e3d7984812e90baa6ea7c0fcaa3e2100
mahabur  rahman <annur4395@gmail.com>
2022-09-26 18:55:55 +0600

    annuouncement added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Announcement />
+      <Navbar />
+    </>
+  );
 };
 
 export default Home;
