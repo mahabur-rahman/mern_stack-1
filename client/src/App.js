18593e734b1215b8330d56aabd46ab9da9d09615
mahabur  rahman <annur4395@gmail.com>
2022-09-27 11:05:56 +0600

    Login page added

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,10 @@ import "./global.css";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Cart from "./pages/Cart";
+import Login from "./pages/Login";
 
 function App() {
-  return <Cart />;
+  return <Login />;
 }
 
 export default App;
