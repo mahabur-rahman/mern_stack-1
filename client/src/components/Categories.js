4566f4c57ea71058de98766f619227746359fa3d
mahabur  rahman <annur4395@gmail.com>
2022-09-27 08:46:00 +0600

    categories item added

---
+++ client/src/components/Categories.js
@@ -0,0 +1,22 @@
+import React from "react";
+import CategoryItem from "./CategoryItem";
+import { categories } from "../Data";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+`;
+
+const Categories = () => {
+  return (
+    <Container>
+      {categories.map((item) => (
+        <CategoryItem key={item.id} item={item} />
+      ))}
+    </Container>
+  );
+};
+
+export default Categories;
