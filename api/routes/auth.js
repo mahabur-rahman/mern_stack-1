e3aeb764442bcf5e2a918e9f10929445dd508407
mahabur  rahman <annur4395@gmail.com>
2022-09-28 17:27:46 +0600

    api login system added

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -22,6 +22,33 @@ router.post("/register", async (req, res) => {
     return res.status(500).json(err);
   }
 });
+
 // LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ username: req.body.username });
+
+    if (!user) {
+      return res.status(401).json("User not found!");
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    const originalPass = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    if (originalPass !== req.body.password) {
+      return res.status(401).json("Wrong credentials!");
+    }
+
+    const { password, ...others } = user._doc;
+
+    return res.status(200).json(others);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
