035c0a104ee05fcf8616708c60a035f0d371eed6
mahabur  rahman <annur4395@gmail.com>
2022-09-28 19:26:23 +0600

    User Statistic mongoDB aggregate logic implement / user count per month

--- api/routes/users.route.js
+++ api/routes/users.route.js
@@ -78,6 +78,34 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+// get monthly user stats
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1)); // 2022 - today
+
+  try {
+    const data = await UserModel.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+
+      {
+        $group: {
+          _id: "$month", // name of month
+          total: { $sum: 1 }, // total number of user register this month
+        },
+      },
+    ]);
+
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 // #################################
 
 // export
