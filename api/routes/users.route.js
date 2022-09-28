d1564948332fd4a1522a86aa27e6c4f9a8d1cde9
mahabur  rahman <annur4395@gmail.com>
2022-09-28 18:54:31 +0600

    Only admin can upate user and token veriyfy with jwt

--- api/routes/users.route.js
+++ api/routes/users.route.js
@@ -1,5 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const CryptoJS = require("crypto-js");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const UserModel = require("../models/User.model");
+
+// UPDATE
+// DELETE
+// GET ALL
+// GET SINGLE USER
+// GET MONTHLY STATS
+
+// #################################
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    return res.status(200).json(updatedUser);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+// #################################
 
 // export
 module.exports = router;
