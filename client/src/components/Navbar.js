cf8a5292dc2fc2f03ad8f41c70d61d099a5f279f
mahabur  rahman <annur4395@gmail.com>
2022-10-02 12:38:41 +0600

    reduxjs/toolkit setup and get quantity

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import { Badge } from "@material-ui/core";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   height: 60px;
@@ -55,6 +56,9 @@ const MenuItem = styled.div`
 `;
 
 const Navbar = () => {
+  const quantity = useSelector((state) => state.cart.quantity);
+  console.log(quantity);
+
   return (
     <Container>
       <Wrapper>
@@ -72,7 +76,7 @@ const Navbar = () => {
           <MenuItem>Register</MenuItem>
           <MenuItem>Login</MenuItem>
           <MenuItem>
-            <Badge badgeContent={4} color="primary">
+            <Badge badgeContent={quantity} color="primary">
               <ShoppingCartOutlined />
             </Badge>
           </MenuItem>
