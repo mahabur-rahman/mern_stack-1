c5ad276be8a8feb5d30b0892348de96fee0be4f1
mahabur  rahman <annur4395@gmail.com>
2022-10-03 19:37:35 +0600

    home page ui added

--- admin/src/pages/home/Home.js
+++ admin/src/pages/home/Home.js
@@ -1,7 +1,27 @@
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
+import { userData } from "../../dummyData";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 
 const Home = () => {
-  return <div className="home">home</div>;
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+        data={userData}
+      />
+
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
