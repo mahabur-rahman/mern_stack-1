dd4eed137002f8c57a8c5cb12a4c9e4e896fe8cc
mahabur  rahman <annur4395@gmail.com>
2022-10-03 17:14:02 +0600

    redux persist implement

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,6 @@ import { useSelector } from "react-redux";
 
 const App = () => {
   const user = useSelector((state) => state.user.currentUser);
-  console.log(user);
 
   return (
     <>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,46 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// export const store = configureStore({
+//   reducer: {
+//     cart: cartReducer,
+//     user: userReducer,
+//   },
+// });
+
+// ############ redux persist ############
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
