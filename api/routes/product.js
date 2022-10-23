79f283b20fd82f740988185581c431f97a9fc325
mahabur  rahman <annur4395@gmail.com>
2022-10-23 20:34:57 +0600

    update/delete/get single / get all product with query logic added

--- api/routes/product.js
+++ api/routes/product.js
@@ -28,5 +28,69 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+// UPDATE PRODUCT
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    return res.status(200).json(updatedProduct);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// DELETE PRODUCT
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await ProductModel.findByIdAndDelete(req.params.id);
+
+    return res.status(200).json("Product has been deleted..");
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// GET SINGLE PRODUCT
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await ProductModel.findById(req.params.id);
+
+    return res.status(200).json(product);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// GET ALL PRODUCT
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+
+  try {
+    let products;
+
+    if (qNew) {
+      products = await ProductModel.find().sort({ createdAt: -1 }).limit(3);
+    } else if (qCategory) {
+      products = await ProductModel.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await ProductModel.find();
+    }
+
+    return res.status(200).json(products);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 // exports
 module.exports = router;
