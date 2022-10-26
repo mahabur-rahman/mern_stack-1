183c57770620df8b7cba11cbeedddf799f44e5ca
mahabur  rahman <annur4395@gmail.com>
2022-10-26 11:03:29 +0600

    get data using query & sorting

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,20 +1,84 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
-const Products = () => {
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  // if cat has
+  useEffect(() => {
+    const getCategoryProducts = async () => {
+      try {
+        const res = await axios.get(
+          cat
+            ? `http://localhost:4000/api/products?category=${cat}`
+            : "http://localhost:4000/api/products"
+        );
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getCategoryProducts();
+  }, [cat]);
+
+  // if filter (color : red, size : s) has
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
+
+  // if user select price range
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) => {
+        return [...prev].sort((a, b) => {
+          return a.createdAt - b.createdAt;
+        });
+      });
+    }
+
+    if (sort === "asc") {
+      setFilteredProducts((prev) => {
+        return [...prev].sort((a, b) => {
+          return a.price - b.price;
+        });
+      });
+    }
+
+    if (sort === "desc") {
+      setFilteredProducts((prev) => {
+        return [...prev].sort((a, b) => {
+          return b.price - a.price;
+        });
+      });
+    }
+  }, [sort]);
+
   return (
     <Container>
-      {popularProducts.map((item) => (
-        <Product item={item} key={item.id} />
-      ))}
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item._id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item.id} />)}
     </Container>
   );
 };
