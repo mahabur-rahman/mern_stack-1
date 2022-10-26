4e4cc39795663b5346413606c6ce919c30cce8ea
mahabur  rahman <annur4395@gmail.com>
2022-10-26 09:42:36 +0600

    routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,43 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 // react router dom
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Pay from "./components/Pay";
 import Success from "./components/Success";
 
 const App = () => {
+  const user = true;
+
   return (
     <Router>
       <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+
+        <Route path="/cart">
+          <Cart />
+        </Route>
+
         <Route path="/pay">
           <Pay />
         </Route>
